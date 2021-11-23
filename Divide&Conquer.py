015ee130692468477c963fe0d349989766fb929e
Malkolm <amouli-0@student.ltu.se>
2021-11-23 21:27:42 +0100

    la in typ creatdata och började lite smått på incemental

--- Divide&Conquer.py
+++ Divide&Conquer.py
@@ -1,1 +1,17 @@
-peep peep poop poop
+#peep peep poop poop
+
+import random
+import math
+import cProfile
+
+def CreateData():
+    dataset = []
+
+    for i in range(1000000):
+        dataset.append(random.randrange(0, 101))    # choose number between 0 and 50 at random
+        #dataset.append(i)                          # fills the dataset with already sorted data
+        # Creates an almost sorted dataset, every tenth loop the input will be randomized
+        #if i % 10 == 0: dataset.append(random.randrange(0, 101))
+        #else: dataset.append(i)
+    #print("test", dataset)
+    return dataset
--- incremental.py
+++ incremental.py
@@ -1,1 +1,40 @@
-peep peep poop poop
+#peep peep poop poop
+
+import random
+import math
+import cProfile
+
+def CreateData():
+    dataset = []
+
+    for i in range(10):
+        dataset.append(random.randrange(0, 101))    # choose number between 0 and 50 at random
+        #dataset.append(i)                          # fills the dataset with already sorted data
+        # Creates an almost sorted dataset, every tenth loop the input will be randomized
+        #if i % 10 == 0: dataset.append(random.randrange(0, 101))
+        #else: dataset.append(i)
+    #print("test", dataset)
+    return dataset
+
+def incremental(list):      #fortfarande insertionsort
+    for i in range(len(list)):
+        if (i <= 2):
+            continue
+        for j in range(2, 0, -1):
+            if i < j:
+                break
+            list.insert(j, list[i])
+            list.pop(i + 1)
+
+        
+        
+    return(list)
+
+def test():
+    data = CreateData()
+    print(data)
+    data = incremental(data)
+    print(data)
+
+test()
+
