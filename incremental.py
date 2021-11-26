4c140527a211d6b6c7d484834cb1e924da50c2f2
Malkolm <amouli-0@student.ltu.se>
2021-11-26 16:51:51 +0100

    rip

--- incremental.py
+++ incremental.py
@@ -16,21 +16,53 @@ def CreateData():
     #print("test", dataset)
     return dataset
 
-def incremental(list):          # väldigt inte klar
-    first3 = list[0:3]
-    list.insert(0, insertionsort(list[0:3]))    #0 till men inte med 3
 
-    for i in range(3, len(list)):
-        flag = 0
-        for j in range(2, 0, -1):
-            if list[i] > list[j]:
-                break
-            elif list[i] < list[j]: #yea fixa nåttt här bra här :)
-                
+def incremental(list):
+    element = list[len(list) -1]
+    if len(list) == 3:
+        return(sort(list))
 
-                list.insert(j, list[i])
-                list.pop(i + 1)
-    return(list)
+    list = incremental(list[:-1])
+
+    if (list[2] > element):
+        if (list[1] > element):
+            if (list[0] > element):
+                list.insert(0, element)
+            else:
+                list.insert(1, element)
+        else:
+            list.insert(2, element)
+    return(list[0:3])
+
+def sort(list):
+
+
+
+
+
+def divide(list):
+    if (len(list) == 3):
+        return(sort(list))
+    list = divide(list[0:-1])
+
+    return(list[0:2])
+
+
+#def incremental(list):          # väldigt inte klar
+#    first3 = list[0:3]
+#    list.insert(0, insertionsort(list[0:3]))    #0 till men inte med 3
+#
+#    for i in range(3, len(list)):
+#        flag = 0
+#        for j in range(2, 0, -1):
+#            if list[i] > list[j]:
+#                break
+#            elif list[i] < list[j]: #yea fixa nåttt här bra här :)
+#                
+#
+#                list.insert(j, list[i])
+#                list.pop(i + 1)
+#    return(list)
 
 
 def insertionsort(list):                # gammla vanliga
