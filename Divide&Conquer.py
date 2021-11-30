c080ff45802c2fa39b1bd6ed8bda64382aad8452
infinitymalle <80246427+infinitymalle@users.noreply.github.com>
2021-11-30 11:11:20 +0100

    break my code

--- Divide&Conquer.py
+++ Divide&Conquer.py
@@ -7,22 +7,23 @@ import cProfile
 def CreateData():
     dataset = []
 
-    for i in range(5*3):
+    for i in range(3*2**2):
         dataset.append(random.randrange(0, 101))    # choose number between 0 and 50 at random
         #dataset.append(i)                          # fills the dataset with already sorted data
         # Creates an almost sorted dataset, every tenth loop the input will be randomized
         #if i % 10 == 0: dataset.append(random.randrange(0, 101))
         #else: dataset.append(i)
     #print("test", dataset)
+    print(len(dataset))
     return dataset
 
 
 def divide(list):
-
-    halv = math.floor(len(list)/2)
-    
+    print(len(list))
+    halv = int(len(list)/2)
     if (len(list) == 3):
         temp = 0
+        print("beep")
         if list[0] > list[1]:
             temp = list[0]
             list[0] = list[1]
@@ -37,24 +38,32 @@ def divide(list):
             list[2] = temp
         return(list)
     else:
-        Llist = divide(list[:halv])
+        Llist = divide(list[0:halv])
         Rlist = divide(list[halv:])
     finalList = []
-    if (len(list) == 3):
-        if Rlist[0] < Llist[1]:
-            finalList.appende(Rlist.pop(0))
-        else:
-            finalList.appende(Llist.pop(0))
-        if Rlist[0] < Llist[0]:
-            finalList.appende(Rlist.pop(0))
-        else:
-            finalList.appende(Llist.pop(0))
-        if Rlist[1] < Llist[0]:
-            finalList.appende(Rlist.pop(0))
-        else:
-            finalList.appende(Llist.pop(0))
 
+    if Rlist[0] < Llist[0]:
+        finalList.append(Rlist[0])
+        Rlist.pop(0)
+    else:
+        finalList.append(Llist[0])
+        Llist.pop(0)
 
+    if Rlist[0] < Llist[0]:
+        finalList.append(Rlist[0])
+        Rlist.pop(0)
+    else:
+        finalList.append(Llist[0])
+        Llist.pop(0)
+
+    if Rlist[0] < Llist[0]:
+        finalList.append(Rlist[0])
+        Rlist.pop(0)
+    else:
+        finalList.append(Llist[0])
+        Llist.pop(0)
+
+    print(finalList) 
     return(finalList)
 
 def test():
