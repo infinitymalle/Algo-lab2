0a5516207b3651926c2de9d2aaf4b7e8ee6d4577
infinitymalle <80246427+infinitymalle@users.noreply.github.com>
2021-11-30 10:49:23 +0100

    divide funkar inte riktigt

--- Divide&Conquer.py
+++ Divide&Conquer.py
@@ -7,7 +7,7 @@ import cProfile
 def CreateData():
     dataset = []
 
-    for i in range(10):
+    for i in range(5*3):
         dataset.append(random.randrange(0, 101))    # choose number between 0 and 50 at random
         #dataset.append(i)                          # fills the dataset with already sorted data
         # Creates an almost sorted dataset, every tenth loop the input will be randomized
@@ -19,7 +19,7 @@ def CreateData():
 
 def divide(list):
 
-    halv = len(list)/2
+    halv = math.floor(len(list)/2)
     
     if (len(list) == 3):
         temp = 0
@@ -37,11 +37,25 @@ def divide(list):
             list[2] = temp
         return(list)
     else:
-        Llist = divide(list[0:halv])
-        Rlist = divide(list[halv:-1])
+        Llist = divide(list[:halv])
+        Rlist = divide(list[halv:])
+    finalList = []
+    if (len(list) == 3):
+        if Rlist[0] < Llist[1]:
+            finalList.appende(Rlist.pop(0))
+        else:
+            finalList.appende(Llist.pop(0))
+        if Rlist[0] < Llist[0]:
+            finalList.appende(Rlist.pop(0))
+        else:
+            finalList.appende(Llist.pop(0))
+        if Rlist[1] < Llist[0]:
+            finalList.appende(Rlist.pop(0))
+        else:
+            finalList.appende(Llist.pop(0))
 
 
-    return(list[0:3])
+    return(finalList)
 
 def test():
     data = CreateData()
